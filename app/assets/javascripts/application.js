0adf34fdef1843118761b66d6bba18af8bf8633d
Eric Kidd <git@randomhacks.net>
2013-10-10 14:23:49 -0400

    Install Ember.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require srs_collector
+
+// for more details see: http://emberjs.com/guides/application/
+SrsCollector = Ember.Application.create();
+
 //= require_tree .
