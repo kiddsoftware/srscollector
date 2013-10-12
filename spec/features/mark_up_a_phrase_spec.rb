c396c0680b2da25ed66d945d4d79104a3c07ebb6
Eric Kidd <git@randomhacks.net>
2013-10-12 09:12:53 -0400

    Use a rich text editor for the card front

--- spec/features/mark_up_a_phrase_spec.rb
+++ spec/features/mark_up_a_phrase_spec.rb
@@ -4,14 +4,35 @@ require "spec_helper"
 feature "Add definitions to a snippet", :js => true do
   scenario "User pastes a phrase and selects a word" do
     visit "/"
-    fill_in "Front", :with => "Je demande pardon aux enfants d'avoir dédié"
+    fill_in_html "#front", with: "suis"
+    select_all "#front"
+    click_button("Lookup")
+    iframe_location.should == "http://fr.wiktionary.org/wiki/suis"
+  end
 
-    # Select "dédié".
-    # Button?
-    # Follow link to "dédier".
-    # Select "Consacrer au culte divin."
-    # Button?
+  def fill_in_html(field, options)
+    with = options[:with]
+    page.execute_script(<<"EOD")
+(function () {
+  var wysihtml5 = $(#{field.to_json}).data("wysihtml5");
+  var editor = wysihtml5.editor;
+  editor.setValue(#{with.to_json});
+})();
+EOD
+  end
 
-    #expect("Back").to have_text("dédier = Consacrer au culte divin.")
+  def select_all(field)
+    page.execute_script(<<"EOD")
+(function () {
+  var wysihtml5 = $(#{field.to_json}).data("wysihtml5");
+  var editor = wysihtml5.editor;
+  var selection = editor.composer.selection;
+  selection.selectNode(selection.doc.documentElement);
+})();
+EOD
+  end
+
+  def iframe_location
+    page.evaluate_script("$('#dictionary').attr('src')")
   end
 end
