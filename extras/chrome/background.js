4cab43a6222829262ef2e1e1bc1ebd7d7dba8cc3
Eric Kidd <git@randomhacks.net>
2013-10-25 07:04:29 -0400

    Chrome extension: Add internal code to support login

--- extras/chrome/background.js
+++ extras/chrome/background.js
@@ -2,6 +2,9 @@
 //chrome.browserAction.setBadgeText({ text: "1" });
 //chrome.browserAction.setBadgeBackgroundColor({ color: "#008" });
 
+// Needed to access our API.  Will be updated once we're logged in.
+var API_KEY = null;
+
 // Confirm that we have saved a text snippet.
 function notifyUser(title, text) {
   var notification = webkitNotifications.createNotification(
@@ -13,13 +16,18 @@ function notifyUser(title, text) {
 // Display a context menu when text is selected.
 function onSaveSelection(info, tab) {
   var selection = info.selectionText;
+  var card = {
+    front: selection,
+    source: tab.title, // Thanks to activeTab permission.
+    source_url: info.pageUrl
+  };
   RSVP.resolve($.ajax({
     url: "http://www.srscollector.com/api/v1/cards.json",
     method: 'POST',
     contentType: "application/json; charset=utf-8",
-    data: JSON.stringify({ card: { front: selection } }),
+    data: JSON.stringify({ card: card, api_key: API_KEY }),
     dataType: 'text' // Handle 201 CREATED responses.
-  })).then(function (json) {
+  })).then(function () {
     notifyUser("Text Saved", selection);
   }).fail(function (reason) {
     notifyUser("Can't Save Text", reason.status);
@@ -27,8 +35,27 @@ function onSaveSelection(info, tab) {
 }
 
 // Install our context menu.
-chrome.contextMenus.create({
-  contexts: ["selection"],
-  title: "Save selection to SRS Collector",
-  onclick: onSaveSelection
-});
+function onSignedIn() {
+  chrome.contextMenus.create({
+    contexts: ["selection"],
+    title: "Save selection to SRS Collector",
+    onclick: onSaveSelection
+  });    
+}
+
+// Sign into our site.
+function signIn(email, password) {
+  var user = { email: email, password: password };
+  RSVP.resolve($.ajax({
+    url: "http://www.srscollector.com/api/v1/users/api_key.json",
+    method: 'POST',
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify({ user: user })
+  })).then(function (json) {
+    API_KEY = json["user"]["api_key"];
+    onSignedIn();
+    notifyUser("Signed In", "Whoo!");
+  }).fail(function (reason) {
+    notifyUser("Sign In Failed", reason.status);
+  });
+}
