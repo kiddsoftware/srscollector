e85d0ee1674725a251e4bb96a1a1d6cf3679a7c0
Eric Kidd <git@randomhacks.net>
2013-11-02 16:06:39 -0400

    Validate media file attachment properties

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -3,4 +3,8 @@ class MediaFile < ActiveRecord::Base
   has_attached_file :file
 
   validates :card, presence: true
+  validates_attachment :file,
+    presence: true,
+    content_type: { content_type: /^image\/(jpg|png|gif)$/ },
+    size: { in: 0..128.kilobytes }
 end
--- spec/models/media_file_spec.rb
+++ spec/models/media_file_spec.rb
@@ -4,4 +4,10 @@ describe MediaFile do
   it { should belong_to(:card) }
   it { should validate_presence_of(:card) }
   it { should have_attached_file(:file) }
+
+  it { should validate_attachment_presence(:file) }
+  it { should validate_attachment_content_type(:file).
+         allowing('image/png', 'image/gif', 'image/jpg').
+         rejecting('text/plain', 'text/html') }
+  it { should validate_attachment_size(:file).less_than(128.kilobytes) }
 end
