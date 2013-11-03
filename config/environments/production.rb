7159f5d7752de425ba61fac1aaf8703352846087
Eric Kidd <git@randomhacks.net>
2013-11-03 01:22:13 -0500

    Configure Paperclip to use S3 in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ SrsCollector::Application.configure do
 
   # Select our Ember.js build.
   config.ember.variant = :production
+
+  # Use S3 to store our file attachments.
+  SrsCollector::Application.config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
