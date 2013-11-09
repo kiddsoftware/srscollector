3f9f515bd6d41ee218d53ed64758afcfdd383dd6
Eric Kidd <git@randomhacks.net>
2013-11-09 12:42:31 -0500

    Anki: Improve source file headers for plugin
    This will normally be destributed independently of our the rest of our
    source tree, in user-editable form.  So let's add annotations to the
    files pointing people to the "Unlicense."

--- extras/anki/SRS_Collector.py
+++ extras/anki/SRS_Collector.py
@@ -2,6 +2,8 @@
 # Author: Eric Kidd <http://kiddsoftware.com/>
 #
 # This is free and unencumbered software released into the public domain.
+# This program comes with ABSOLUTELY NO WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # For more information, please refer to <http://unlicense.org/>
 
 # TODO:
