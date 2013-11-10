ea1bbf92b66b9a8ef9f2230bc86ac6917bd3a754
Eric Kidd <git@randomhacks.net>
2013-11-10 08:15:45 -0500

    Try escaping reserved SQL words differently

--- app/models/card_model.rb
+++ app/models/card_model.rb
@@ -1,6 +1,6 @@
 class CardModel < ActiveRecord::Base
-  has_many :card_model_fields, -> { order("'order' ASC") }
-  has_many :card_model_templates, -> { order("'order' ASC") }
+  has_many :card_model_fields, -> { order('"order" ASC') }
+  has_many :card_model_templates, -> { order('"order" ASC') }
   has_many :cards
 
   validates :short_name, presence: true, uniqueness: true
