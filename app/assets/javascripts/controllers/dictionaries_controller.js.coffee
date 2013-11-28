4cf356819f48f1e9961f05cbb174697beb9eb4dc
Eric Kidd <git@randomhacks.net>
2013-11-28 16:33:40 -0500

    Keep dictionary choice stable even when no dictionary is available

--- app/assets/javascripts/controllers/dictionaries_controller.js.coffee
+++ app/assets/javascripts/controllers/dictionaries_controller.js.coffee
@@ -9,11 +9,15 @@ SrsCollector.DictionariesController = Ember.ArrayController.extend
   language: null
   currentDictionary: null
 
+  # Used to keep track of the dictionary when none is shown.  We should
+  # probably make this per-language at some point, perhaps by creating
+  # a languages controller and item controllers for each language.
+  previousDictionary: null
+
   # Find only those dictionaries which are useful for the current language.
   filtered: (->
     content = @get("arrangedContent")
     language = @get("language")
-    console.log("filtered", content, language)
     return [] unless content? && language?
     content.filter (d) ->
       d.get("fromLanguage") == language
@@ -28,9 +32,18 @@ SrsCollector.DictionariesController = Ember.ArrayController.extend
 
   filteredChanged: (->
     filtered = @get("filtered")
-    if filtered.get("length") > 0
+    current = @get("currentDictionary")
+    previous = @get("previousDictionary")
+    if current? && filtered.contains(current)
+      # It's valid, so leave it alone.
+    else if previous? && filtered.contains(previous)
+      # It's valid, so restore it.
+      @set("currentDictionary", previous)
+      @set("previousDictionary", null)
+    else if filtered.get("length") > 0
       @set("currentDictionary", filtered.objectAt(0))
     else
+      @set("previousDictionary", current) if current
       @set("currentDictionary", null)
   ).observes("filtered", "filtered.@each")
 
--- spec/features/language_detection_spec.rb
+++ spec/features/language_detection_spec.rb
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 require "spec_helper"
+# Needed to prevent "Circular dependency detected while autoloading
+# constant Card" when run under guard.
+require_relative "../../app/controllers/api/v1/cards_controller"
 
 feature "Detect language of entered text", :js => true do
+
+  let(:text_en) do
+    <<EOD
+Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.
+EOD
+  end
+
+  let(:text_fr) do
+    <<EOD
+Pour que le caractère d'un être humain dévoile des qualités vraiment exceptionnelles, il faut avoir la bonne fortune de pouvoir observer son action pendant de longues années.
+EOD
+  end
+
   let!(:en) { FactoryGirl.create(:language, iso_639_1: "en", name: "English") }
   let!(:fr) { FactoryGirl.create(:language, iso_639_1: "fr", name: "Français") }
 
@@ -18,26 +34,44 @@ feature "Detect language of entered text", :js => true do
                        name: "Wiktionnaire",
                        from_language: fr,
                        to_language: fr,
-                       url_pattern: 'http://fr.wiktionary.org/wiki/%s')
+                       url_pattern: 'http://fr.wiktionary.org/wiki/%s',
+                       score: 1)
+  end
+
+  let!(:images) do
+    FactoryGirl.create(:dictionary,
+                       name: "Image Search",
+                       from_language: fr,
+                       to_language: nil,
+                       url_pattern: 'http://www.example.com?q=%s',
+                       score: 0.80)
   end
 
   before { default_card_model_for_spec; visit "/"; sign_up }
 
   scenario "User enters text, and the language is detected" do
     page.should have_select("Studying:", selected: "(auto-detect language)")
-    fill_in_html "#front", with: <<EOD
-Pour que le caractère d'un être humain dévoile des qualités vraiment exceptionnelles, il faut avoir la bonne fortune de pouvoir observer son action pendant de longues années.
-EOD
+    fill_in_html "#front", with: text_fr
     page.should have_select("Studying:", selected: "Français")
     page.should have_select("Dictionary:", selected: "Wiktionnaire")
 
     click_button("Next")
 
     page.should have_select("Studying:", selected: "(auto-detect language)")
-    fill_in_html "#front", with: <<EOD
-Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.
-EOD
+    fill_in_html "#front", with: text_en
     page.should have_select("Studying:", selected: "English")
     page.should have_select("Dictionary:", selected: "Wiktionary")
+  end
+
+  scenario "Dictionary remains stable even when not available" do
+    fill_in_html "#front", with: text_fr
+    select("Image Search")
+    page.should have_select("Dictionary:", selected: "Image Search")
+
+    fill_in_html "#front", with: ""
+    page.should_not have_select("Dictionary:", selected: "Image Search")
+
+    fill_in_html "#front", with: text_fr
+    page.should have_select("Dictionary:", selected: "Image Search")
   end
 end
