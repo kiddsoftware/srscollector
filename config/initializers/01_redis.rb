c49552e00d960910d487261c961ff826963ba735
Eric Kidd <git@randomhacks.net>
2013-12-01 15:39:58 -0500

    Don't load Redis or rollout when buildind assets on Heroku

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,7 +1,12 @@
-# Connect to Redis.
-begin
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(host: uri.host, port: uri.port)
-  # Always put the test environment in a high-numbered database.
-  REDIS.select(15) if Rails.env.test?
+# Connect to Redis if we have a URL.  If we don't, we're probably building
+# assets on Heroku.
+if ENV["REDISTOGO_URL"]
+ begin
+   uri = URI.parse(ENV["REDISTOGO_URL"])
+   REDIS = Redis.new(host: uri.host, port: uri.port)
+   # Always put the test environment in a high-numbered database.
+   REDIS.select(15) if Rails.env.test?
+ end
+else
+  REDIS = nil
 end
--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,10 +1,15 @@
-ROLLOUT = Rollout.new(REDIS)
+if REDIS
 
-# We also use rollout to distinguish between paid and unpaid accounts, so
-# we can make certain features available only to paid users.
-ROLLOUT.define_group(:supporters) do |user|
-  user.supporter?
+  ROLLOUT = Rollout.new(REDIS)
+
+  # We also use rollout to distinguish between paid and unpaid accounts, so
+  # we can make certain features available only to paid users.
+  ROLLOUT.define_group(:supporters) do |user|
+    user.supporter?
+  end
+
+  # Features the client needs to know about.
+  ROLLOUT_CLIENT_FEATURES = [:playable_media]
+  
 end
 
-# Features the client needs to know about.
-ROLLOUT_CLIENT_FEATURES = [:playable_media]
